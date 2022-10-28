a4a9a3657d8763f643705bf2b363cb72d986169a
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-10-28 09:02:59 -0300

    👌 IMPROVE: Code-splitting in bundle implemented + Prefetcher Link component

--- react-client/src/routes.tsx
+++ react-client/src/routes.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
+import React from 'react';
 
 // This file implements File-System routing for the client react app,
-// and you should not need to edit it.
+// and you should not need to edit it. Includes code-splitting in the final bundle
+// Made thanks to @oedotme on github!
 
 type RoutesType = {
     [key: string]: { default: React.ElementType }
@@ -11,16 +13,16 @@ type PreservedRoutesType = {
     '404'?: React.ElementType
 }
 
-const ROUTES: RoutesType = import.meta.glob("/src/pages/**/[a-z[]*.tsx", { eager: true })
+const ROUTES = import.meta.glob("/src/pages/**/[a-z[]*.tsx")
 const PRESERVED: RoutesType = import.meta.glob('/src/pages/404.tsx', { eager: true })
 
-const routes = Object.keys(ROUTES).map((route) => {
+export const routes = Object.keys(ROUTES).map((route) => {
     const path = route
         .replace(/\/src\/pages|index|\.tsx$/g, '')
         .replace(/\[\.{3}.+\]/, '*')
         .replace(/\[(.+)\]/, ':$1')
 
-    return { path, component: ROUTES[route].default }
+    return { path, component: React.lazy(ROUTES[route] as () => Promise<{ default: React.ComponentType }>), preload: ROUTES[route] }
 })
 
 const preserved: PreservedRoutesType = Object.keys(PRESERVED).reduce((preserved, file) => {
@@ -32,11 +34,13 @@ export const AppRoutes = () => {
     const NotFound = preserved?.['404']
 
     return (
-        <Routes>
-            {routes.map(({ path, component: Component }) => (
-                <Route key={path} path={path} element={Component ? <Component /> : <></>} />
-            ))}
-            <Route path='*' element={NotFound ? <NotFound /> : <></>} />
-        </Routes>
+        <React.Suspense fallback={'Loading...'}>
+            <Routes>
+                {routes.map(({ path, component: Component }) => (
+                    <Route key={path} path={path} element={Component ? <Component /> : <></>} />
+                ))}
+                <Route path='*' element={NotFound ? <NotFound /> : <></>} />
+            </Routes>
+        </React.Suspense>
     )
 }
